10f0b6ef5ab65e6490dea0488c58353560a90b73
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2023-11-02 09:54:54 +0530

    Update general-staffing.js
    remove itc logo

--- pages/general-staffing.js
+++ pages/general-staffing.js
@@ -424,9 +424,7 @@ export default function Home() {
                         <div className="logoc wow fadeInUp delay-0-2s">
                            <img src="/assets/img/logo/hml/DABUR.png" alt='DABUR'/>
                         </div>
-                        // <div className="logoc wow fadeInUp delay-0-2s">
-                        //    <img src="/assets/img/logo/hml/ITC.png" alt='ITC'/>
-                        // </div>
+                   
                         <div className="logoc wow fadeInUp delay-0-2s">
                            <img src="/assets/img/logo/hml/LAKME.png" alt='LAKME'/>
                         </div>
