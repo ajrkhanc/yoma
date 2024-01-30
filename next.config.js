090b8330da59409d26cebc61ce5ae42de8a13ef1
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-30 15:13:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ module.exports = {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin',
           },
+           {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
         ],
       },
     ];
