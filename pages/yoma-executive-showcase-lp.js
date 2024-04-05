ec47ca1cbb0ac2e26239911afe66e3f0944e99bd
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-05 14:30:12 +0530

    Update yoma-executive-showcase-lp.js

--- pages/yoma-executive-showcase-lp.js
+++ pages/yoma-executive-showcase-lp.js
@@ -9,7 +9,7 @@ export default function YomaExecutiveShowcase(){
         xhttp.onload = function () {
             console.log(this.responseText);
         }
-        xhttp.open("Post", 'https://ajrkhan.xyz/yomamultinational/wp-json/contact-form-7/v1/contact-forms/23/feedback');
+        xhttp.open("Post", 'https://byldgroup.in/yomamultinational/wp-json/contact-form-7/v1/contact-forms/23/feedback');
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4) {
